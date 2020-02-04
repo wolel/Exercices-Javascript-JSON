30ea6ce49200df69e7e03d8af854885732ef8bd4
wolel <56022602+wolel@users.noreply.github.com>
2020-02-04 15:37:53 +0100

    apli meteo

--- exercice.js
+++ exercice.js
@@ -1,22 +1,43 @@
-var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-if (this.readyState === 4 && this.status === 200){
-    console.log(this.responseText);
-    var json = JSON.parse(this.responseText);
-
-    document.getElementById('date').innerHTML = DateDuJour (date)+" " + date.getDate()+" " +(NomDuMois(date))+" "+date.getFullYear();
-    //document.getElementById('ville').innerHTML = "Ville: "+ json.name;
-    document.getElementById('temp_actuelle').innerHTML = "température actuelle: " + Math.round(json.main.temp);
-
-    document.getElementById('temp_max').innerHTML = "Température max: "+ Math.round(json.main.temp_max)+ "°C";
-    document.getElementById('temp_min').innerHTML = "Température max: "+ Math.round(json.main.temp_min)+ "°C";
-    document.getElementById('pres').innerHTML = "Pres Atmosferique: "+ Math.round(json.main.pressure);
-    document.getElementById('wind').innerHTML = "Vitesse du vent: "+ Math.round(json.main.speed) + "km/h";
-    document.getElementById('humidite').innerHTML = "Humidité: "+ Math.round(json.main.humidity) + "%";
+var name = document.getElementById('ville');
+var btn = document.getElementById('btn_ville');
 
 
+function start() {
+
+    var input = document.getElementById('ville').value;
+
+    var xhttp = new XMLHttpRequest();
+    //console.log(xhttp);
+
+    xhttp.onreadystatechange = function () {
+        console.log(input +'OK bien recu !');
+
+        if (this.readyState === 4 && this.status === 200) {
+            //console.log(this.responseText);
+
+
+            var json = JSON.parse(this.responseText);
+            console.log(json);
+            let date = new Date( json.timezone*1000);
+
+                document.getElementById('date').innerHTML = date;
+                document.getElementById('ville').innerHTML = "Ville: " + json.name;
+                document.getElementById('tempMax').innerHTML = "Température max : " + Math.round(json.main.temp_max) + "°C";
+                document.getElementById('tempMin').innerHTML = "Température min : " + Math.round(json.main.temp_min) + "°C";
+                document.getElementById('pressAtm').innerHTML = "Pression atmosphérique : " + json.main.pressure + " Pa";
+                document.getElementById('wind').innerHTML = "Vitesse du vent : " + json.wind.speed + " km/h";
+                document.getElementById('humidite').innerHTML = "Humidité : " + json.main.humidity + " %";
+
+
+
+
+
+        }
+    };
+
+    xhttp.open('GET', "index.php?ville=" +input, true);
+    console.log(xhttp.open);
+    xhttp.send();
 }
-};
-xhttp.open('GET', "index.php?ville="+input,true);
-xhttp.send();
+    btn.addEventListener('click', start);
--- index.php
+++ index.php
@@ -2,11 +2,36 @@
 /**
  * Voir docu sur: https://www.php.net/manual/fr/function.curl-init.php
  */
+$input = $_GET['ville'];
+$url = "http://api.openweathermap.org/data/2.5/weather?q=".$input."&units=metric&APPID=df33c6a5f3ff83ba9958a30a417f5d2f";
+//$url = "http://api.openweathermap.org/data/2.5/forecast?q=charleroi&units=metric&APPID=df33c6a5f3ff83ba9958a30a417f5d2f";
+
+
+
+//echo json_encode($url);
+
+
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://home.openweathermap.org/");
 
+// configuration des options
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
+
+
+/*
+if(curl_exec($ch) === false)
+{
+    echo 'Erreur Curl : ' . curl_error($ch);
+}
+else
+{
+    echo 'L\'opération s\'est terminée sans aucune erreur';
+}
+*/
+// exécution de la session
 curl_exec($ch);
+
+// fermeture des ressources
 curl_close($ch);
 
